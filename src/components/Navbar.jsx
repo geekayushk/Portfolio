da05ba3b07801c6c981bb9509ffc2f8d2a152471
Ayush Kumar <kumarayush0427@gmail.com>
2024-02-24 20:07:26 +0530

    resume update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -144,7 +144,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://drive.google.com/drive/folders/1bKIo8duToo5_BSX8TpYV4sZQ8ub8SCnQ?usp=sharing"
+              href="https://drive.google.com/file/d/1rfQrJOKG6YF9LMXuPklydz7JBbgNrHRf/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
