dadb3cb05257dd8ac5aa24b319ef7ed0a8a3682f
Ayush Kumar <kumarayush0427@gmail.com>
2024-04-30 02:24:41 +0530

    work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,7 +6,7 @@ import shopinn from "../assets/shopinn.jpg";
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto my-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
